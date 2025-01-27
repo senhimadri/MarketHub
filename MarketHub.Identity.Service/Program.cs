c28e948a471c8686bda45f3c21f9b9ad63a8149c
senhimadri <senhimadri.dev@gmail.com>
2025-01-27 15:10:55 +0600

    feat(Result Pattern) : Implement result and structure new result idea.
    - Create a common library
    - Implement a common structure for return and response type

--- MarketHub.Identity.Service/Program.cs
+++ MarketHub.Identity.Service/Program.cs
@@ -24,20 +24,23 @@ app.UseSwaggerUI();
 
 app.MapPost("/register", async (IRegistrationService registrationService, CreateIdentityUserDto user) =>
 {
-    await registrationService.RegisterUserAsync(user);
-    return Results.Created();
+    var response = await registrationService.RegisterUserAsync(user);
+
+    return response.Match(onSuccess: () => Results.Ok("User created successfully"),
+                onValidationFailure: (validationErrors) => Results.ValidationProblem(validationErrors),
+                onFailure: (error) => Results.Problem(error.Description));
 });
 
 app.MapPost("/login", async (ILoginService loginService, LoginDto request) =>
 {
     var response = await loginService.LoginAsync(request);
-    return Results.Ok(response);
+    return response is not null ? Results.Ok(response) : Results.NotFound();
 });
 
 app.MapPost("/refresh-token", async (ILoginService loginService, string refreshToken) =>
 {
     var response = await loginService.RefreshTokenAsync(refreshToken);
-    return Results.Ok(response);
+    return response is not null ? Results.Ok(response) : Results.NotFound();
 });
 
 app.Run();

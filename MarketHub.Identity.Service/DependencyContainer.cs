173d4ceffe68211d3c528665dca6eaa6328b683a
senhimadri <senhimadri.dev@gmail.com>
2025-01-27 16:16:52 +0600

    feat (refactor) : Grouping the minimal Api

---
+++ MarketHub.Identity.Service/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using MarketHub.Identity.Service.Repositories.Login;
+using MarketHub.Identity.Service.Repositories.Registration;
+using MarketHub.Identity.Service.Repositories.Token;
+
+namespace MarketHub.Identity.Service;
+
+public static class DependencyContainer
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        services.AddTransient<ITokenUtils, TokenUtils>();
+        services.AddScoped<IRegistrationService, RegistrationService>();
+        services.AddScoped<ILoginService, LoginService>();
+        return services;
+    }
+}

9f402e3654ade75c1dbfd519680c90a02b75ea35
senhimadri <senhimadri.dev@gmail.com>
2025-01-27 15:10:55 +0600

    feat(Result Pattern) : Implement result and structure new result idea.
    - Create a common library
    - Implement a common structure for return and response type

---
+++ MarketHub.Common.Library/OperationResults/Errors.cs
@@ -0,0 +1,17 @@
+﻿namespace MarketHub.Common.Library.OperationResult;
+
+public sealed record Error(int Code, string? Description = null)
+{
+    public static readonly Error None = new(200, string.Empty);
+}
+
+public class Errors
+{
+    public static readonly Error ContentNotFound = new Error(404, "Content not found.");
+    public static readonly Error BadRequest = new Error(400, "Bad request.");
+    public static readonly Error InternalServerError = new Error(500, "Internal server error.");
+    public static readonly Error UnauthorizedAccess = new Error(401, "Unauthorized access.");
+    public static readonly Error Forbidden = new Error(403, "Forbidden access.");
+    public static readonly Error ValidationFailed = new Error(422, "Validation failed.");
+    public static Error NewError(int code, string message) => new Error(code, message);
+}
---
+++ MarketHub.Common.Library/OperationResults/OperationResult.cs
@@ -0,0 +1,25 @@
+﻿namespace MarketHub.Common.Library.OperationResult;
+
+public class OperationResult
+{
+    protected OperationResult(bool isSuccess, Error error, Dictionary<string, string[]>? validationErrors = null)
+    {
+        if (isSuccess && error != Error.None || !isSuccess && error == Error.None)
+            throw new ArgumentException("Invalid error.");
+
+        IsSuccess = isSuccess;
+        Error = error;
+
+        ValidationErrors = validationErrors;
+    }
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+    public readonly Dictionary<string, string[]>? ValidationErrors;
+    public bool IsValidationFailure => ValidationErrors?.Any() == true;
+    public static OperationResult Success() => new(true, Error.None);
+    private static OperationResult Failure(Error error) => new(false, error);
+    public static OperationResult ValidationFailure(Dictionary<string, string[]> errors) => new(false, Errors.ValidationFailed, errors);
+    public static implicit operator OperationResult(Error error) => Failure(error);
+}
+

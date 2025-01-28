3d1417a998cbf4d2c3b20bd3d35902e00b41a6c9
senhimadri <senhimadri.dev@gmail.com>
2025-01-28 13:10:24 +0600

    feature:implement JWT Bearer  in common library
    - Create a common service extender to implement JWT Bearer commonly.

---
+++ MarketHub.Common.Library/Identity/JwtAuthenticationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace MarketHub.Common.Library.Auth;
+
+public static class JwtAuthenticationExtensions
+{
+    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var key = configuration["Jwt:Key"];
+        var issuer = configuration["Jwt:Issuer"];
+        var audience = configuration["Jwt:Audience"];
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                        .AddJwtBearer(options =>
+                        {
+                            options.TokenValidationParameters = new TokenValidationParameters
+                            {
+                                ValidateIssuer = true,
+                                ValidIssuer = issuer, 
+
+                                ValidateAudience = true,
+                                ValidAudience = audience, 
+
+                                ValidateIssuerSigningKey = true,
+                                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key!)),
+
+                                ValidateLifetime = true, 
+                                ClockSkew = TimeSpan.Zero 
+                            };
+                        });
+
+        return services;
+    }
+}
